d2895e068813cb10da0dff704e494b1546c8fd52
E-Justin <67754579+E-Justin@users.noreply.github.com>
2022-08-27 10:29:11 -0700

    Create models.py

---
+++ twitter_api/flask/twitter/src/models.py
@@ -0,0 +1,82 @@
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+# create db adapter object
+db =    SQLAlchemy()
+
+# create User class as a subclass of SQL ALchemy's db.Model class
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)  # int + autoincrement = SERIAL dtype
+    username = db.Column(db.String(128), unique = True, nullable = False)  # 128 character limit on username: VARCHAR dtype
+    password = db.Column(db.String(128), nullable = False)  # 128 character limit on pw: VARCHAR dtype
+
+    tweets = db.relationship('Tweet', backref = 'user', cascade = "all,delete")
+
+    def __init__(self, username: str, password: str):
+        self.username = username
+        self.password = password
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'username': self.username
+        }
+
+likes_table = db.Table(
+    'likes',
+    db.Column(
+        'user_id', db.Integer,
+        db.ForeignKey('users.id'),
+        primary_key = True
+    ),
+
+    db.Column(
+        'tweet_id', db.Integer,
+        db.ForeignKey('tweets.id'),
+        primary_key = True
+    ),
+
+    db.Column(
+        'created_at', db.DateTime,
+        default = datetime.datetime.utcnow,
+        nullable = False
+    )
+)
+
+    
+
+# create Tweet model
+class Tweet(db.Model):
+    __tablename__ = 'tweets'
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    content = db.Column(db.String(280), nullable = False)
+    created_at = db.Column(
+        db.DateTime,
+        default = datetime.datetime.utcnow,
+        nullable = False
+    )
+    # every tweet must have exactly 1 user_id linked to it
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
+
+    liking_users = db.relationship(
+        'User', secondary = likes_table,
+        lazy = 'subquery',
+        backref = db.backref('liked_tweets', lazy = True)
+    )
+
+    def __init__(self, content: str, user_id: int):
+        self.content = content
+        self.user_id = user_id
+        # left out id and created_at bc they will be auto populated
+
+    def serialize(self):
+        return {
+            'id': self.content,
+            'content': self.content,
+            'created_at': self.created_at.isoformat(),
+            'user_id': self.user_id
+        }
+
+
+
